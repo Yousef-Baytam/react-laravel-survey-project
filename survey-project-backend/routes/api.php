36a77d60ce1bdce4398f774c4c50ee1ab176f481
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-16 21:25:14 +0300

    Added routes to be used

--- survey-project-backend/routes/api.php
+++ survey-project-backend/routes/api.php
@@ -22,3 +22,27 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('me', 'me');
     Route::post('refresh', 'refresh');
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'admin'], function () {
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::group(['middleware' => 'isAdmin'], function () {
+                Route::group(['prefix' => 'forms'], function () {
+                    Route::get('/', [FormsController::class, 'getAllForms']);
+                    Route::post('/new', [AdminFormsController::class, 'addForms']);
+                    Route::patch('/{id}', [AdminFormsController::class, 'updateForms']);
+                    Route::delete('/{id}', [AdminFormsController::class, 'deleteForms']);
+                });
+            });
+        });
+    });
+    Route::group(['prefix' => 'user'], function () {
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::group(['prefix' => 'forms'], function () {
+                Route::get('/', [FormsController::class, 'getAllForms']);
+                Route::get('/answered', [FormsController::class, 'getAllAnsweredForms']);
+                Route::post('/submit', [FormsFormsController::class, 'answerForms']);
+            });
+        });
+    });
+});
