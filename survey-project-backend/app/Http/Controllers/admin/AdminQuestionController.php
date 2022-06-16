1343434a7b066627f33d5f2fbc72668f60b896ab
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-16 23:00:30 +0300

    Added a function to create a question

--- survey-project-backend/app/Http/Controllers/admin/AdminQuestionController.php
+++ survey-project-backend/app/Http/Controllers/admin/AdminQuestionController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use app\Models\Question;
+use App\Models\Question_type;
 
 class AdminQuestionController extends Controller
 {
-    //
+
+    public function addQuestion(Request $request, $id)
+    {
+        Question::create([
+            'question' => $request->question,
+            'surveys_id' => $id,
+            'question_type_id' => Question_type::where('question_type', $request->question_type)->get()[0]->id
+        ]);
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
