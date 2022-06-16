563fc1c50c59d92441de0586f46a3b3e4541095d
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-16 22:49:07 +0300

    Updated get all forms funtion to get send all the questions as well

--- survey-project-backend/app/Http/Controllers/FormController.php
+++ survey-project-backend/app/Http/Controllers/FormController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Survey;
-use App\Mdoels\Question;
+use App\Models\Question;
 
 class FormController extends Controller
 {
     public function getAllForms()
     {
-        $surveys = Survey::with('questions')->with('question_types')->get();
+        $surveys = Survey::with('questions')->get();
+        $questions = Question::with('values')->with('question_types')->with('surveys')->get();
         return response()->json([
             "status" => "Success",
-            "res" => $surveys
+            "surveys" => $surveys,
+            "questions" => $questions
         ], 200);
     }
 }
