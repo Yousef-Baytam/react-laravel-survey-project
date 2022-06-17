c634fd81363bcd8ad5f8597799290e1c0166c193
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-18 02:03:40 +0300

    Added a button to add a question

--- survey-project-frontend/src/pages/AddForm.js
+++ survey-project-frontend/src/pages/AddForm.js
@@ -10,17 +10,7 @@ export default function AddForm() {
 
     const [formName, setFormName] = useState('')
     const [formDescription, setFormDescription] = useState('')
-    const [questions, setQuestions] = useState([{
-        'type': 'Drop Down',
-        'value': 'xyz',
-        'question': 'how dumb are you?'
-    }, {
-        'type': 'Single Line',
-        'question': 'how dumb are you?1'
-    }, {
-        'type': 'MCQ',
-        'question': 'how dumb are you?2'
-    }])
+    const [questions, setQuestions] = useState([{}])
 
     const handleQuestion = (i, value) => {
         if (i.type === 'Drop Down')
@@ -40,6 +30,14 @@ export default function AddForm() {
             </div>
             <div>
                 <Multiline name={'formName'} placeholder={'Form Title'} value={formDescription} setValue={setFormDescription} />
+            </div>
+            <div>
+                <select>
+                    <option value={'Single Line'}>Single Line</option>
+                    <option value={'Drop Down'}>Drop Down</option>
+                    <option value={'MCQ'}>MCQ</option>
+                </select>
+                <button onClick={() => { setQuestions([...questions, { 'type': document.querySelector('select').value }]) }}>Add Question</button>
             </div>
             <div>
                 <List questions={questions} setQuestions={setQuestions} handleQuestion={handleQuestion} />
