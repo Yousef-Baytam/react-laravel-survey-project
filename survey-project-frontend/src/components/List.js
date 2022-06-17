07d90bcfbeed559992d7221ef06fd2d83fa0612e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-18 01:46:25 +0300

    Adjusted the function to render different components

--- survey-project-frontend/src/components/List.js
+++ survey-project-frontend/src/components/List.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function List(props) {
-    // return (questions.map((i) => <i.name  id={${ i.id }} values={${ i.values }}/>))
+    return props.questions.map((i) => <div key={i.question}>{props.handleQuestion(i, i.value && i.value)}</div>)
 }
--- survey-project-frontend/src/pages/AddForm.js
+++ survey-project-frontend/src/pages/AddForm.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react'
+import Drop from '../components/Drop'
 import Input from '../components/Input'
 import List from '../components/List'
 import Multiline from '../components/Multiline'
+import Radio from '../components/Radio'
+import Single from '../components/Single'
 
 export default function AddForm() {
 
     const [formName, setFormName] = useState('')
     const [formDescription, setFormDescription] = useState('')
     const [questions, setQuestions] = useState([{
-        'type': 'Drop down',
+        'type': 'Drop Down',
+        'value': 'xyz',
         'question': 'how dumb are you?'
+    }, {
+        'type': 'Single Line',
+        'question': 'how dumb are you?1'
+    }, {
+        'type': 'MCQ',
+        'question': 'how dumb are you?2'
     }])
 
-    const handleQuestion = () => {
-        for (let i of questions) {
-            if (i.type == 'Drop Down')
-                return <Drop />
-            if (i.type == 'Signle Line')
-                return <Single />
-            if (i.type == 'MCQ')
-                return <Radio />
-            if (i.type == 'MCQ')
-                return <Radio />
-        }
+    const handleQuestion = (i, value) => {
+        if (i.type === 'Drop Down')
+            return <Drop value={value} />
+        if (i.type === 'Single Line')
+            return <Single />
+        if (i.type === 'MCQ')
+            return <Radio />
+        if (i.type === 'MCQ')
+            return <Radio />
     }
 
     return (
