20c5694a52e6de72b1bf6ba54477f3f8fab43c55
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-18 01:31:09 +0300

    Attempted to create addForm

---
+++ survey-project-frontend/src/components/List.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function List(props) {
+    // return (questions.map((i) => <i.name  id={${ i.id }} values={${ i.values }}/>))
+}
--- survey-project-frontend/src/pages/AddForm.js
+++ survey-project-frontend/src/pages/AddForm.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react'
 import Input from '../components/Input'
+import List from '../components/List'
 import Multiline from '../components/Multiline'
 
 export default function AddForm() {
+
     const [formName, setFormName] = useState('')
     const [formDescription, setFormDescription] = useState('')
+    const [questions, setQuestions] = useState([{
+        'type': 'Drop down',
+        'question': 'how dumb are you?'
+    }])
+
+    const handleQuestion = () => {
+        for (let i of questions) {
+            if (i.type == 'Drop Down')
+                return <Drop />
+            if (i.type == 'Signle Line')
+                return <Single />
+            if (i.type == 'MCQ')
+                return <Radio />
+            if (i.type == 'MCQ')
+                return <Radio />
+        }
+    }
+
     return (
         <div>
             <div>
@@ -13,7 +33,9 @@ export default function AddForm() {
             <div>
                 <Multiline name={'formName'} placeholder={'Form Title'} value={formDescription} setValue={setFormDescription} />
             </div>
+            <div>
+                <List questions={questions} setQuestions={setQuestions} handleQuestion={handleQuestion} />
+            </div>
         </div>
-
     )
 }
