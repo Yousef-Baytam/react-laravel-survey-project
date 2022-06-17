fba05f942ebbc20506ae71576e90c235d19c4b9c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-17 13:41:53 +0300

    Created a token loggin check

--- survey-project-frontend/src/App.js
+++ survey-project-frontend/src/App.js
@@ -1,6 +1,33 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+    let token
+    if (!localStorage.getItem('token'))
+      return
+    token = localStorage.getItem('token')
+    let data = new FormData()
+    async function getUser() {
+      const user = await axios({
+        url: "http://127.0.0.1:8000/api/me",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${ token }`
+        },
+        data: data,
+      })
+      if (user.data)
+        setLoggedIn(true)
+    }
+    getUser()
+  }, [])
+
   return (
     <div className="App">
       hello
