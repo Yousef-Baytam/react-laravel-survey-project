b08fa4eae852ba2fd054134db3da84a7a8a41cfb
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-18 21:17:22 +0300

    Coppied the dropdown component to mcq and adjusted it accordigly

--- survey-project-frontend/src/components/Radio.js
+++ survey-project-frontend/src/components/Radio.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import axios from 'axios'
 
-export default function Radio() {
+export default function Radio(props) {
+    const currentUser = useSelector((state) => state.user)
+
+    const [options, setOptions] = useState([])
+    const [optionText, setOptionText] = useState('')
+
+    const handleRadio = () => {
+        return (
+            options.map((i) => (<>
+                <input style={{ width: '2em' }} type={'radio'} name={props.num} id={i.id} key={i.id} value={i.value} />
+                <label htmlFor={i.id}>{i.value}</label>
+                <br />
+            </>))
+        )
+    }
+
+    const createValue = async (id, value) => {
+        console.log(id, value)
+        let data = new FormData()
+        data.append('value', value ?? '')
+        try {
+            let res = await axios({
+                url: `http://127.0.0.1:8000/api/v1/admin/values/new/${ id }`,
+                method: "POST",
+                headers: {
+                    Authorization: `bearer ${ localStorage.getItem('token') }`
+                },
+                data: data,
+            })
+            console.log(res)
+            setOptions([...options, { 'value': optionText, 'id': res.data.value.id }])
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <div>Radio</div>
+        <div>
+            <label htmlFor={props.question}>{props.question}</label>
+            {currentUser.user.payload.user_type == 'admin' && props.admin(props.num)}
+            <br />
+            <input style={{ width: '20%', }} onChange={(e) => setOptionText(e.target.value)} value={optionText}></input>
+            <button onClick={() => { createValue(props.num, optionText); }}>Add option</button><br />
+            <div>
+                {handleRadio()}
+            </div>
+        </div>
     )
 }
