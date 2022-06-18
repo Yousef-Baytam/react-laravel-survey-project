69a52ad5511d82f9c30526f890c9e72ba3ea8166
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-18 23:54:57 +0300

    Added functiont to get the form and its questions

--- survey-project-frontend/src/pages/FormView.js
+++ survey-project-frontend/src/pages/FormView.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function FormView() {
+export default function FormView(props) {
     const formId = parseInt(window.location.href.slice(window.location.href.length - 1))
+
+    const [form, setForm] = useState({})
+    const [formQuestions, setFormQuestions] = useState([])
+
+    const getForm = () => {
+        for (let i of props.forms)
+            i.id === formId && setForm(i)
+    }
+
+    const getFormQuestions = () => {
+        if (!form.id)
+            return
+        console.log('hello')
+        console.log(form.questions)
+        for (let i of form.questions)
+            for (let j of props.questions)
+                console.log(j)
+        // i.id == j.id && setFormQuestions([...formQuestions, j])
+    }
+
+    useEffect(() => {
+        getForm()
+    }, [])
+
+    useEffect(() => {
+        getFormQuestions()
+    }, [form])
 
     return (
         <div>FormView</div>
