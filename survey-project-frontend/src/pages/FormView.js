20af0f9e9edea62b4ba56b671027e94503e99b62
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-19 00:40:31 +0300

    Fixed questions setState

--- survey-project-frontend/src/pages/FormView.js
+++ survey-project-frontend/src/pages/FormView.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
 export default function FormView(props) {
-    const formId = parseInt(window.location.href.slice(window.location.href.length - 1))
+    const formId = parseInt(window.location.href.slice(window.location.href.indexOf('forms/') + 6))
 
     const [form, setForm] = useState({})
     const [formQuestions, setFormQuestions] = useState([])
+    const [answers, setAnswers] = useState([])
 
     const getForm = () => {
         for (let i of props.forms)
@@ -14,12 +15,11 @@ export default function FormView(props) {
     const getFormQuestions = () => {
         if (!form.id)
             return
-        console.log('hello')
-        console.log(form.questions)
+        let arr = []
         for (let i of form.questions)
             for (let j of props.questions)
-                console.log(j)
-        // i.id == j.id && setFormQuestions([...formQuestions, j])
+                i.id == j.id && arr.push(j)
+        setFormQuestions(arr)
     }
 
     useEffect(() => {
