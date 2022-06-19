0e275f5c9f63ca373f9dda14988377923f917a8f
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-19 20:55:20 +0300

    Deleted the unescesary redux store theme edits

--- survey-project-frontend/src/features/userSlice.js
+++ survey-project-frontend/src/features/userSlice.js
@@ -4,25 +4,16 @@ export const userSlice = createSlice({
     name: 'user',
     initialState: {
         user: {},
-        theme: 'lightblue'
     },
     reducers: {
         storeUser: (state, data) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.user = data
-        },
-        storeTheme: (state, data) => {
-            state.theme = data
         },
     },
 })
 
 // Action creators are generated for each case reducer function
 export const currentUser = (state) => state.user.value
-export const currentTheme = (state) => state.theme.value
 
 export const { storeUser } = userSlice.actions
 
